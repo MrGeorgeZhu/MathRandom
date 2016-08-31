c8b606e84c989b66c15cd40d12d751856fa3e5bc
GeorgeZhu <GeorgeZhu@10.0.0.9>
2016-08-30 22:03:45 -0600

    Finished the code at home

--- Math.radom/src/Math/mathradom.java
+++ Math.radom/src/Math/mathradom.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class mathradom
 	{
 		static int numberOfQuestions;
+		static int num1;
+		static int num2;
 		public static void main(String[] args)
 			{
 				Scanner userInput = new Scanner(System.in);
@@ -14,10 +16,19 @@ public class mathradom
 			    Scanner userInput2 = new Scanner(System.in);
 			    System.out.println("How many questions would you like to practise, " + name + "?" );
 			    numberOfQuestions = userInput2.nextInt();
-
+			    q1();
+			    q2();
 			}
-
 		public static void q1(){
+			random();
 			System.out.println("Given int randomNumber = (int)(Math.random( ) * " + num1 + ") + " + num2 +";");
 		}
+		public static void q2(){
+			random();
+			System.out.println("Given int randomNumber = (int)(Math.random( ) * " + num1 + ") + " + num2 +";");
+		}
+		public static void  random(){
+	         num1 = (int)(Math.random()*10)+1;
+	         num2 = (int)(Math.random()*10)+1;
+	    }
 	}
